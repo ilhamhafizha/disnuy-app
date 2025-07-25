9364bace0b0f034d504d575a3e9e0e9cdd9072ad
ilhamhafizha <muhammadilhamhafizha@gmail.com>
2025-07-25 15:49:04 +0700

    feat: create page movies, tv series, and search

--- src/pages/Movies/index.tsx
+++ src/pages/Movies/index.tsx
@@ -1,7 +1,32 @@
+import BannerDetail from "../../components/BannerDetail";
+import BannerMask from "../../components/BannerMask";
+import ContentCard from "../../components/ContentCard";
+import ImageBanner from "../../components/ImageBanner";
+import ScrollableSection from "../../components/ScrollableSection";
+import SectionItems from "../../components/ScrollableSection/SectionItems";
+
 const Movies = () => {
   return (
-    <div style={{ color: "white", textAlign: "center", padding: "20px" }}>
-      <h1>Movies Page</h1>
+    <div>
+      <ImageBanner />
+      <BannerDetail />
+      <BannerMask>
+        <ScrollableSection title="Popular Movies on Disney+">
+          {Array(12)
+            .fill(0)
+            .map((_) => (
+              <SectionItems>
+                <ContentCard
+                  title="Mickey Mouse"
+                  description="Mickey Mouse is an animated cartoon character co-created in 1928 by Walt Disney and Ub Iwerks."
+                  posterImage="https://image.tmdb.org/t/p/w342/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg"
+                  bannerImage="https://image.tmdb.org/t/p/w342/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg"
+                  onClick={() => console.log("Content Card Clicked")}
+                />
+              </SectionItems>
+            ))}
+        </ScrollableSection>
+      </BannerMask>
     </div>
   );
 };
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,7 +1,29 @@
+import ContentCard from "../../components/ContentCard";
+import styles from "./index.module.css";
+
 const Search = () => {
   return (
-    <div style={{ color: "white", textAlign: "center", padding: "20px" }}>
-      <h1>Search Page</h1>
+    <div>
+      <div className={styles.searchBar}>
+        <span style={{ color: "white" }} className="material-symbols-outlined">
+          Search
+        </span>
+        <input type="text" placeholder="Movies, shows and more" />
+      </div>
+
+      <div className={styles.contentGrid}>
+        {Array(12)
+          .fill(0)
+          .map((_) => (
+            <ContentCard
+              title="Mickey Mouse"
+              description="Mickey Mouse is an animated cartoon character co-created in 1928 by Walt Disney and Ub Iwerks."
+              posterImage="https://image.tmdb.org/t/p/w342/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg"
+              bannerImage="https://image.tmdb.org/t/p/w342/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg"
+              onClick={() => console.log("Content Card Clicked")}
+            />
+          ))}
+      </div>
     </div>
   );
 };
